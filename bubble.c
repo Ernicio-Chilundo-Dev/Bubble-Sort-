403608c58b090fbef376c8a8c7b7edd339dab4cd
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-07-19 04:31:58 +0200

    Add function to swap two strings in a 2D character array

--- bubble.c
+++ bubble.c
@@ -6,3 +6,13 @@
 #define MAX_WORDS 20
 #define MAX_LENGTH 20
 
+
+// Function to swap two strings
+
+void swap(char words[MAX_WORDS][MAX_LENGTH], int i, int j)
+{
+    char temp[MAX_LENGTH];
+    strcpy(temp, words[i]);
+    strcpy(words[i], words[j]);
+    strcpy(words[j], temp);
+}
