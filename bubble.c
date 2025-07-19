345ad43b17d557fa75f19ba8d0cb1216e182816f
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-07-19 15:21:43 +0200

    Add bubble_sort function to sort array of strings using Bubble Sort - Uses strcmp to compare strings - Swaps strings with helper swap function - Prints array after each swap to show sorting progress

--- bubble.c
+++ bubble.c
@@ -16,3 +16,27 @@ void swap(char words[MAX_WORDS][MAX_LENGTH], int i, int j)
     strcpy(words[i], words[j]);
     strcpy(words[j], temp);
 }
+
+// Function to apply Bubble sort
+
+void bubble_sort(char words[MAX_WORDS][MAX_LENGTH], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1; j++)
+        {
+            if (strcmp(words[j], words[j + 1]) > 0)
+            {
+                swap(words, j, j + 1);
+
+                // Show sorting steps
+                printf("-> ");`
+                for (int k = 0; k < n; k++)
+                {
+                    printf("%s", words[k]);
+                }
+                printf("\n");
+            }
+        }
+    }
+}
