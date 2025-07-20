3b505b2ce3edcf032f90e3157fbeb52775cf1ccd
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-07-20 08:08:42 +0200

    add space it the printf('%s ', words[i]);

--- bubble.c
+++ bubble.c
@@ -90,7 +90,7 @@ int main(void)
 
         for (int i = 0; i < count; i++)
         {
-            printf("%s", words[i]);
+            printf("%s ", words[i]);
         }
         printf("\n");
     }
